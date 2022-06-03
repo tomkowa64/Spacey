79321be6eb997e32d3b3e861818ccb7505d794f7
Tomasz Kowalski <tomkowa64@gmail.com>
2022-06-03 17:43:45 +0200

    Login page

---
+++ lib/views/start.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Start extends StatelessWidget {
+  const Start({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+          padding: const EdgeInsets.only(top: 20),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/Spacey-login-bg.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          SizedBox(
+                              width: 100,
+                              height: 150,
+                              child: Image(
+                                  image: AssetImage(
+                                      "assets/images/icons/foil-space-blanket-512.png")))
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        child: Text(
+                          "LOGIN",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 24,
+                              letterSpacing: 3,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.only(top: 20),
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        child: Text(
+                          "Email",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromRGBO(194, 194, 194, 0.7)),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.only(top: 10),
+                          padding: EdgeInsets.only(left: 30, right: 30),
+                          child: TextFormField(
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white))),
+                          ))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.only(top: 20),
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        child: Text(
+                          "Hasło",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromRGBO(194, 194, 194, 0.7)),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.only(top: 10),
+                          padding: EdgeInsets.only(left: 30, right: 30),
+                          child: TextFormField(
+                            obscureText: true,
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white))),
+                          ))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(top: 30),
+                          child: SizedBox(
+                            width: 180,
+                            height: 50,
+                            child: TextButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all(
+                                        BeveledRectangleBorder(
+                                            side: BorderSide(
+                                                color: Colors.white,
+                                                width: 0.5,
+                                                style: BorderStyle.solid))),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Color.fromRGBO(42, 59, 113, 1))),
+                                child: const Text(
+                                  "LOGIN",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      letterSpacing: 1),
+                                )),
+                          ))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 100),
+                              width: MediaQuery.of(context).size.width,
+                              child: Container(
+                                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          Text(
+                                            "About Spacey",
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                color: Color.fromRGBO(
+                                                    135, 135, 135, 1)),
+                                          )
+                                        ]),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        TextButton(
+                                            onPressed: () {},
+                                            style: ButtonStyle(
+                                                shape: MaterialStateProperty.all<
+                                                        RoundedRectangleBorder>(
+                                                    RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(
+                                                                    100))),
+                                                backgroundColor:
+                                                    MaterialStateProperty.all<
+                                                            Color>(
+                                                        Color.fromRGBO(
+                                                            87, 136, 239, 1))),
+                                            child: Text(
+                                              "+",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 40),
+                                            ))
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ))
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
